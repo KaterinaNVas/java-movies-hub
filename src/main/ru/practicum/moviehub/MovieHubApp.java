b0b6b7886a16d8b2bf4cfd24400e066a5baafcfe
KaterinaNVas <leb.kati@yandex.ru>
2026-09-20 22:08:11 +0300

    feat: add MovieHub application entry point

--- src/main/ru/practicum/moviehub/MovieHubApp.java
+++ src/main/ru/practicum/moviehub/MovieHubApp.java
@@ -1,12 +1,15 @@
-//package ru.practicum.moviehub;
-//
-//import ru.practicum.moviehub.http.MoviesServer;
-//import ru.practicum.moviehub.store.MoviesStore;
-//
-//public class MovieHubApp {
-//    public static void main(String[] args) {
-//        final MoviesServer server = new MoviesServer(new MoviesStore(), 8080);
-//        Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
-//        server.start();
-//    }
-//}
+package ru.practicum.moviehub;
+
+import ru.practicum.moviehub.http.MoviesServer;
+
+public class MovieHubApp {
+    public static void main(String[] args) {
+        MoviesServer server = new MoviesServer();
+
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(server::stop)
+        );
+
+        server.start();
+    }
+}
